1554d7d7410f7d278fbec6a25ae420c7cfbad4b5
Bellanov Apilli <bellanovapilli@gmail.com>
2025-05-02 23:40:14 -0400

    Updated error function logic

--- examples/hello.sh
+++ examples/hello.sh
@@ -11,12 +11,8 @@
 #######################################
 err() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
-}
-
-if ! do_something; then
-  err "Unable to do_something"
   exit 1
-fi
+}
 
 
 echo "Hello World"
