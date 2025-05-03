d4ead36eb53fa981924a716c8101272807b33351
Bellanov Apilli <bellanovapilli@gmail.com>
2025-05-02 23:31:34 -0400

    Added function description to script

--- examples/hello.sh
+++ examples/hello.sh
@@ -2,4 +2,21 @@
 #
 # Initial Template Example.
 
+#######################################
+# Display an error message to STDERR.
+# Globals:
+#   None
+# Arguments:
+#   String containing the error message.
+#######################################
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
+if ! do_something; then
+  err "Unable to do_something"
+  exit 1
+fi
+
+
 echo "Hello World"
